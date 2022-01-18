4188a5bdbef3be9a85477f6ddf746956e9c880b2
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-01-18 20:12:53 +0700

    fix code

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"'edge equals with sqr, expected true', 100, 225, 6", "'edge not equals with sqr, expected true', 200, 300, 3"})
+    @CsvSource(value = {"'edge equals with sqr, expected true', 100, 225, 6", "'edge not equals with sqr, expected true', 200, 300, 3",
+            "'sqr outside, expected true', 25, 81, 0", "'all sqr inside edge, expected true', 81, 9802, 90"})
     void shouldCalculate(String testName, int downEdge, int upEdge, int expected) {
         SQRService service = new SQRService();
 
